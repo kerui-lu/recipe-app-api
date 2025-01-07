5de936bb82dbe155e4d9f868883e0b603aee37c2
keruilu <keruilu@stanford.edu>
2025-01-06 23:13:48 -0800

    implement update ingredients feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -62,7 +62,7 @@ class RecipeSerializer(serializers.ModelSerializer):
         """Handle getting or creating ingredients as needed."""
         auth_user = self.context["request"].user
         for ingredient in ingredients:
-            ingredient_obj, create = Ingredient.objects.get_or_create(
+            ingredient_obj, created = Ingredient.objects.get_or_create(
                 user=auth_user,
                 **ingredient,
             )
@@ -81,9 +81,13 @@ class RecipeSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         """Update recipe."""
         tags = validated_data.pop("tags", None)
+        ingredients = validated_data.pop('ingredients', None)
         if tags is not None:
             instance.tags.clear()
             self._get_or_create_tags(tags, instance)
+        if ingredients is not None:
+            instance.ingredients.clear()
+            self._get_or_create_tags(ingredients, instance)
 
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
