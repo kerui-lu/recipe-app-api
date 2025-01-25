dcbe632ae2f599eec578f07c1f41c1eedf241af0
keruilu <keruilu@stanford.edu>
2025-01-24 22:57:18 -0800

    add IPV4 DNS to ALLOWED_HOSTS

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,16 +21,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
+SECRET_KEY = os.environ.get("SECRET_KEY", "changeme")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.environ.get('DEBUG', 0)))
+DEBUG = bool(int(os.environ.get("DEBUG", 0)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "ec2-13-52-180-112.us-west-1.compute.amazonaws.com",  # EC2 Public IPv4 DNS
+    "127.0.0.1",
+    "localhost",
+]
+
 ALLOWED_HOSTS.extend(
     filter(
         None,
-        os.environ.get('ALLOWED_HOSTS', '').split(','),
+        os.environ.get("ALLOWED_HOSTS", "").split(","),
     )
 )
 
